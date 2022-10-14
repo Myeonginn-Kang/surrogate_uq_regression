835e07890d9e539e20b2b4f239072260bef7f602
Myeonginn-Kang <61176358+Myeonginn-Kang@users.noreply.github.com>
2022-10-14 10:30:23 +0900

    Update uncertainty_score.py

--- uncertainty_score.py
+++ uncertainty_score.py
@@ -28,23 +28,23 @@ def datafree_kd_score(net, test_loader, dname, seed, n_layers, T=10):
     student_training = False
     if not os.path.isfile(('./student/st_%s_%d_%d_0.pt')%(dname, seed, n_layers)):
         student_training = True
-    score = np.mean([(datafree_kd(net, test_loader, student_path='./student/st_%s_%d_%d_%d.pt'%(dname, seed, n_layers, T),student_training=student_training) 
-    - inference(net, test_loader)) ** 2 for _ in range(T)], 0)
+    score = np.mean([(datafree_kd(net, test_loader, student_path='./student/st_%s_%d_%d_%d.pt'%(dname, seed, n_layers, i),student_training=student_training) 
+    - inference(net, test_loader)) ** 2 for i in range(T)], 0)
 
     return score
 
 def ensemble_score(net, test_loader, X_tst, dname, seed, n_layers, student_training=False, T = 10):
 
-    uncertainty1 = perturbation_score(net, test_loader)      
-    uncertainty2 = gradnorm_score(net, X_tst)
+    score1 = perturbation_score(net, test_loader)      
+    score2 = gradnorm_score(net, X_tst)
     set_dropout(net, drop_rate=0.05)
-    uncertainty3 = mc_dropout_score(net, test_loader)
+    score3 = mc_dropout_score(net, test_loader)
     set_dropout(net, drop_rate=0)
     student_training = False
     if not os.path.isfile(('./student/st_%s_%d_%d_0.pt')%(dname, seed, n_layers)):
         student_training = True
-    uncertainty4 = np.mean([(datafree_kd(net, test_loader, student_path='./student/st_%s_%d_%d_%d.pt'%(dname, seed, n_layers, T),student_training=student_training) 
-    - inference(net, test_loader)) ** 2 for _ in range(T)], 0)
-    uncertainty = np.sqrt(uncertainty1) + np.sqrt(uncertainty2) + np.sqrt(uncertainty3) + np.sqrt(uncertainty4)
+    score4 = np.mean([(datafree_kd(net, test_loader, student_path='./student/st_%s_%d_%d_%d.pt'%(dname, seed, n_layers, i),student_training=student_training) 
+    - inference(net, test_loader)) ** 2 for i in range(T)], 0)
+    score = np.sqrt(score1) + np.sqrt(score2) + np.sqrt(score3) + np.sqrt(score4)
 
-    return uncertainty
+    return score
